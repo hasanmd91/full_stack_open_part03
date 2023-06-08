b5ab4b8be86abef989f581d057772ba2377cabb2
hasan md <hasan.md@quanturi.com>
2023-06-08 13:53:04 +0300

    Exercises 3.7.-3.8

--- index.js
+++ index.js
@@ -1,66 +1,25 @@
 const express = require('express');
+const phoneBookData = require('./src/Data/index.js');
+const {
+  parseName,
+  parseNumber,
+  generateRandomId,
+} = require('./src/Utils/index.js');
+const morgan = require('morgan');
+
+const unknowsEndPoint = (req, res, next) => {
+  res.status(404).send({ error: 'unknown end point' });
+};
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 const app = express();
 app.use(express.json());
-
-const phoneBookData = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-const isString = (str) => {
-  if (typeof str !== 'string') {
-    throw new Error(`typeof ${str} is not valid`);
-  }
-  return str;
-};
-
-const checkUnique = (name) => {
-  const matchedData = phoneBookData.find((data) => data.name === name);
-  console.log(name);
-  if (matchedData) {
-    throw new Error(`provided name ${name} must be unique`);
-  }
-  return name;
-};
-
-const parseName = (name) => {
-  if (!name || !isString(name) || !checkUnique(name)) {
-    throw new Error('name is missing');
-  }
-  return name;
-};
-
-const parseNumber = (number) => {
-  if (!number || !isString(number)) {
-    throw new Error('number is missing');
-  }
-  return number;
-};
-
-function generateRandomId(length) {
-  let min = Math.pow(10, length);
-  let max = Math.pow(10, length + 1) - 1;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+app.use(
+  morgan(
+    ':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'
+  )
+);
 
 app.get('/api/persons', (req, res) => {
   res.json(phoneBookData);
@@ -119,6 +78,8 @@ app.post('/api/persons', (req, res) => {
     res.status(404).json({ error: `${err.message}` });
   }
 });
+
+app.use(unknowsEndPoint);
 
 const port = 3001;
 app.listen(port, () => {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,39 @@
+const phoneBookData = require('../Data/index.js');
+
 const isString = (str) => {
   if (typeof str !== 'string') {
     throw new Error(`typeof ${str} is not valid`);
   }
+  return str;
+};
+
+const checkUnique = (name) => {
+  const matchedData = phoneBookData.find((data) => data.name === name);
+  console.log(name);
+  if (matchedData) {
+    throw new Error(`provided name ${name} must be unique`);
+  }
+  return name;
 };
 
 const parseName = (name) => {
-  if (!name || name.length < 0 || isString(name)) {
-    throw new Error(' name is missing');
+  if (!name || !isString(name) || !checkUnique(name)) {
+    throw new Error('name is missing');
   }
+  return name;
 };
 
 const parseNumber = (number) => {
-  if (!number || number.length < 0 || isString(number)) {
-    throw new Error(' name is missing');
+  if (!number || !isString(number)) {
+    throw new Error('number is missing');
   }
+  return number;
 };
+
+function generateRandomId(length) {
+  let min = Math.pow(10, length);
+  let max = Math.pow(10, length + 1) - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = { parseName, parseNumber, generateRandomId };
