d70a84d8f1d5c5ef7fa072d1f06f3404319cabf9
hasan md <hasan.md@quanturi.com>
2023-06-08 15:15:02 +0300

    merge utils to the root index

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 import express from 'express';
 import morgan from 'morgan';
 import { phoneBookData } from './src/Data/index.js';
-import { parseName, parseNumber, generateRandomId } from './src/Utils/index.js';
+
+const isString = (str) => {
+  if (typeof str !== 'string') {
+    throw new Error(`typeof ${str} is not valid`);
+  }
+  return str;
+};
+
+const checkUnique = (name) => {
+  const matchedData = phoneBookData.find((data) => data.name === name);
+  console.log(name);
+  if (matchedData) {
+    throw new Error(`provided name ${name} must be unique`);
+  }
+  return name;
+};
+
+const parseName = (name) => {
+  if (!name || !isString(name) || !checkUnique(name)) {
+    throw new Error('name is missing');
+  }
+  return name;
+};
+
+const parseNumber = (number) => {
+  if (!number || !isString(number)) {
+    throw new Error('number is missing');
+  }
+  return number;
+};
+
+function generateRandomId(length) {
+  let min = Math.pow(10, length);
+  let max = Math.pow(10, length + 1) - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const unknowsEndPoint = (req, res, next) => {
   res.status(404).send({ error: 'unknown end point' });
