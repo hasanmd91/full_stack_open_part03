4d8603125e0081e7d841f4a8c1b5315ec005ec76
hasan md <hasan.md@quanturi.com>
2023-06-08 15:10:02 +0300

    update exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,22 +16,24 @@ const checkUnique = (name) => {
   return name;
 };
 
-export const parseName = (name) => {
+const parseName = (name) => {
   if (!name || !isString(name) || !checkUnique(name)) {
     throw new Error('name is missing');
   }
   return name;
 };
 
-export const parseNumber = (number) => {
+const parseNumber = (number) => {
   if (!number || !isString(number)) {
     throw new Error('number is missing');
   }
   return number;
 };
 
-export function generateRandomId(length) {
+function generateRandomId(length) {
   let min = Math.pow(10, length);
   let max = Math.pow(10, length + 1) - 1;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export { parseName, parseNumber, generateRandomId };
